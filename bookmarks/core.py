94fd71adcad0636926b63d47e9fbc363d32f2d8f
Kyle Laker <kyle@laker.email>
2018-10-07 10:49:14 -0400

    Add support for an API key
    The API key is stored in key.json in $CWD.
    
    Resolves #2

--- bookmarks/core.py
+++ bookmarks/core.py
@@ -9,17 +9,26 @@ from flask import (
 
 app = Flask(__name__)
 bookmark_file = 'bookmarks.json'
+key_file = "key.json"
 bookmarks = []
+key = ""
+err_response = {'error': 'Invalid key'}
 
 @app.before_first_request
 def load_bookmarks():
     global bookmarks
+    global key
     with open(bookmark_file, "r") as bookmark_data:
         bookmarks = json.load(bookmark_data)
+
+    with open(key_file, "r") as key_data:
+        key = json.load(key_data)['key']
+
 
 @app.route("/")
 def hello():
     return render_template("page.html", bookmarks=bookmarks)
+
 
 @app.route("/api/bookmarks", methods=['GET', 'POST', 'PUT'])
 def api():
@@ -27,14 +36,22 @@ def api():
     if request.method == 'GET':
         return json.dumps(bookmarks)
 
-    print(request.data)
+    request_data = json.loads(request.data)
+    if (request_data['key'] != key):
+        return json.dumps(err_response);
 
     if request.method == 'PUT':
-        bookmarks = json.loads(request.data)
+        bookmarks = request_data['bookmarks']
     elif request.method == 'POST':
-        bookmarks += json.loads(request.data)
+        for bookmark in request_data['bookmarks']:
+            if bookmarks not in bookmarks:
+                bookmarks.append(bookmark)
 
+    # Write out the bookmark data so it will be there when the app launches
+    # next
     with open(bookmark_file, "w+") as bookmark_out:
         json.dump(bookmarks, bookmark_out)
 
-    return json.dumps(bookmarks)
+    response = {'error': None, 'bookmarks': bookmarks}
+
+    return json.dumps(response)
